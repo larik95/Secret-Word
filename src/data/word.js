c66a1fc0f8100ef5eadadbac36c96c344bd78272
Larissa <larissaferreira_@live.com>
2022-06-07 11:56:57 -0300

    SecretWordApp

---
+++ src/data/word.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Marcha", "Pneu", "Antena"],
+    fruta: ["Banana", "Morango", "Pera", "Manga", "Laranja"],
+    corpo: ["Braço", "Perna", "Intestino", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React"],
+    alimento: ["Arroz", "Lasanha", "Carne", "Leite", "Ovo"],
+  };
